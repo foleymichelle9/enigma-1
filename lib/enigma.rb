edc4d1ed3cf858bdaca325532bf7417769e38adb
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-28 12:44:14 -0700

    Add generate random key method.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,4 +11,10 @@ class Enigma
   def alphabet
   ("a".."z").to_a << " "
   end
+
+  def generate_random_key
+    five_random_digits = (0..9).to_a.sample(5)
+    five_random_digits.join.to_i
+  end
+
 end
