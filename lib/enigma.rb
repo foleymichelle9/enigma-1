67aa0bc1a8b3d9c356425a99b8ddcb756bb66df0
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-28 12:50:51 -0700

    Add add_shift_to_indices method.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -70,4 +70,16 @@ class Enigma
     find_message_indices(message).each_slice(4).to_a
   end
 
+  def add_shift_to_indices(message)
+    slices = []
+    sliced = slice_indices(message)
+    sliced.map do |slice|
+      slices << slice[0] + find_shifts[:a_shift] if !slice[0].nil?
+      slices << slice[1] + find_shifts[:b_shift] if !slice[1].nil?
+      slices << slice[2] + find_shifts[:c_shift] if !slice[2].nil?
+      slices << slice[3] + find_shifts[:d_shift] if !slice[3].nil?
+    end
+    slices
+  end
+
 end
