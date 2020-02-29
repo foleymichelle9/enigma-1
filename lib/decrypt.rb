b04d49c38dde70bf59053d762fac3c074da81080
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 15:14:57 -0700

    Add subtract_shift_from_indices method

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,4 +1,16 @@
 require_relative 'cipher'
 
 class Decrypt < Cipher
+  
+  def subtract_shift_from_indices(message)
+    slices = []
+    sliced = slice_indices(message)
+    sliced.map do |slice|
+      slices << slice[0] - find_shifts[:a_shift] if !slice[0].nil?
+      slices << slice[1] - find_shifts[:b_shift] if !slice[1].nil?
+      slices << slice[2] - find_shifts[:c_shift] if !slice[2].nil?
+      slices << slice[3] - find_shifts[:d_shift] if !slice[3].nil?
+    end
+    slices
+  end
 end
