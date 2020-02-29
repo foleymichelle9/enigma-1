6450cc75b7acea58b0ea9c188921af14752bf4b7
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 14:41:48 -0700

    Add alphabet test for encrypt

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -11,4 +11,38 @@ class EncryptTest < Minitest::Test
   def test_it_exists
     assert_instance_of Encrypt, @encrypt
   end
+
+  def test_it_can_return_alphabet_array
+    expected =  [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+      " "
+    ]
+
+    assert_equal expected, @encrypt.alphabet
+  end
 end
