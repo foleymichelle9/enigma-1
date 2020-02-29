9949f58db8164dd52c3c4a5f0f9416474fb36e2c
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 15:06:57 -0700

    Add alphabet test

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -11,4 +11,38 @@ class DecryptTest < Minitest::Test
   def test_it_exists
     assert_instance_of Decrypt, @decrypt
   end
+
+  def test_it_can_return_alphabet_array
+    expected =  [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+      " "
+    ]
+
+    assert_equal expected, @decrypt.alphabet
+  end
 end
