7b52edce4561b5724b19b23384a2cf2adce3364b
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 14:01:15 -0700

    Reformat tests to like tests are near one another

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -109,13 +109,31 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.add_shift_to_indices("HELLO WORLD")
   end
 
-  def test_it_can_find_indices_in_alphabet_array_range
+  def test_it_can_subtract_shifts_from_indices
+    Date.stubs(:today).returns(Date.new(1995, 8, 4))
+    @enigma.stubs(:generate_random_key).returns("02715")
+    expected = [7, -23, -70, -16, 14, -1, -59, -13, 17, -16, -51]
+
+    assert_equal expected, @enigma.subtract_shift_from_indices("keder ohulw")
+    assert_equal expected, @enigma.subtract_shift_from_indices("KEDER OHULW")
+  end
+
+  def test_it_can_find_encryption_indices_in_alphabet_array_range
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
     @enigma.stubs(:generate_random_key).returns("02715")
     expected = [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22]
 
-    assert_equal expected, @enigma.indices_in_alphabet_array("hello world")
-    assert_equal expected, @enigma.indices_in_alphabet_array("HELLO WORLD")
+    assert_equal expected, @enigma.encryption_indices_in_alphabet_array("hello world")
+    assert_equal expected, @enigma.encryption_indices_in_alphabet_array("HELLO WORLD")
+  end
+
+  def test_it_can_find_decryption_indices_in_alphabet_array
+    Date.stubs(:today).returns(Date.new(1995, 8, 4))
+    @enigma.stubs(:generate_random_key).returns("02715")
+    expected = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
+
+    assert_equal expected, @enigma.decryption_indices_in_alphabet_array("keder ohulw")
+    assert_equal expected, @enigma.decryption_indices_in_alphabet_array("KEDER OHULW")
   end
 
   def test_it_can_find_encryted_letters
@@ -124,6 +142,14 @@ class EnigmaTest < Minitest::Test
 
     assert_equal "keder ohulw", @enigma.find_encryted_letters("hello world")
     assert_equal "keder ohulw", @enigma.find_encryted_letters("HELLO WORLD")
+  end
+
+  def test_it_can_find_decrypted_letters
+    Date.stubs(:today).returns(Date.new(1995, 8, 4))
+    @enigma.stubs(:generate_random_key).returns("02715")
+
+    assert_equal "hello world", @enigma.find_decrypted_letters("keder ohulw")
+    assert_equal "hello world", @enigma.find_decrypted_letters("KEDER OHULW")
   end
 
   def test_it_can_create_encryption_hash_creation
@@ -139,6 +165,19 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.encryption_hash_creation("HELLO WORLD")
   end
 
+  def test_it_can_get_decryption_hash_creation
+    Date.stubs(:today).returns(Date.new(1995, 8, 4))
+    @enigma.stubs(:generate_random_key).returns("02715")
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.decryption_hash_creation("keder ohulw")
+    assert_equal expected, @enigma.decryption_hash_creation("KEDER OHULW")
+  end
+
   def test_test_it_can_encrypt_message_with_key_and_date
     expected = {
         encryption: "keder ohulw",
@@ -148,6 +187,17 @@ class EnigmaTest < Minitest::Test
 
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
     assert_equal expected, @enigma.encrypt("HELLO WORLD", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_message_with_key_and_date
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+    assert_equal expected, @enigma.decrypt("KEDER OHULW", "02715", "040895")
   end
 
   def test_it_can_encrypt_message_with_key
@@ -163,59 +213,6 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, encrypted
   end
 
-  def test_it_can_encrypt_message_with_random_key
-    @enigma.stubs(:generate_random_key).returns("02715")
-    Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    expected = {
-        encryption: "keder ohulw",
-        key: "02715",
-        date: "040895"
-      }
-
-    assert_equal expected, @enigma.encrypt("hello world")
-    assert_equal expected, @enigma.encrypt("HELLO WORLD")
-  end
-  #==================================DECRYPTION===================================
-
-  def test_it_can_subtract_shifts_from_indices
-    Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @enigma.stubs(:generate_random_key).returns("02715")
-    expected = [7, -23, -70, -16, 14, -1, -59, -13, 17, -16, -51]
-
-    assert_equal expected, @enigma.subtract_shift_from_indices("keder ohulw")
-    assert_equal expected, @enigma.subtract_shift_from_indices("KEDER OHULW")
-  end
-
-  def test_it_can_find_decryption_indices_in_alphabet_array
-    Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @enigma.stubs(:generate_random_key).returns("02715")
-    expected = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
-
-    assert_equal expected, @enigma.decryption_indices_in_alphabet_array("keder ohulw")
-    assert_equal expected, @enigma.decryption_indices_in_alphabet_array("KEDER OHULW")
-  end
-
-  def test_it_can_find_decrypted_letters
-    Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @enigma.stubs(:generate_random_key).returns("02715")
-
-    assert_equal "hello world", @enigma.find_decrypted_letters("keder ohulw")
-    assert_equal "hello world", @enigma.find_decrypted_letters("KEDER OHULW")
-  end
-
-  def test_it_can_get_decryption_hash_creation
-    Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @enigma.stubs(:generate_random_key).returns("02715")
-    expected = {
-        decryption: "hello world",
-        key: "02715",
-        date: "040895"
-      }
-
-      assert_equal expected, @enigma.decryption_hash_creation("keder ohulw")
-      assert_equal expected, @enigma.decryption_hash_creation("KEDER OHULW")
-  end
-
   def test_it_can_decrypt_message_with_key
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
     encrypted = @enigma.encrypt("hello world", "02715")
@@ -228,14 +225,16 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.decrypt(encrypted[:encryption], "02715")
   end
 
-  def test_it_can_decrypt_message_with_key_and_date
+  def test_it_can_encrypt_message_with_random_key
+    @enigma.stubs(:generate_random_key).returns("02715")
+    Date.stubs(:today).returns(Date.new(1995, 8, 4))
     expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-    }
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
 
-    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
-    assert_equal expected, @enigma.decrypt("KEDER OHULW", "02715", "040895")
+    assert_equal expected, @enigma.encrypt("hello world")
+    assert_equal expected, @enigma.encrypt("HELLO WORLD")
   end
 end
