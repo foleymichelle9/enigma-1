9579f17f23c603ca30954fef133e497287eaa5cb
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 15:04:53 -0700

    Create base decrypt class and test

---
+++ lib/decrypt.rb
@@ -0,0 +1,4 @@
+require_relative 'cipher'
+
+class Decrypt < Cipher
+end
---
+++ test/decrypt_test.rb
@@ -0,0 +1,8 @@
+require_relative 'test_helper'
+require 'mocha/minitest'
+require './lib/decrypt'
+require 'date'
+
+class DecryptTest < Minitest::Test
+
+end
