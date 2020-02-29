3d793a43645a0255c99fa65ed7fcaca08fdd2e97
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 15:15:59 -0700

    Add decryption_indices_in_alphabet_array method

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,7 +1,7 @@
 require_relative 'cipher'
 
 class Decrypt < Cipher
-  
+
   def subtract_shift_from_indices(message)
     slices = []
     sliced = slice_indices(message)
@@ -12,5 +12,17 @@ class Decrypt < Cipher
       slices << slice[3] - find_shifts[:d_shift] if !slice[3].nil?
     end
     slices
+  end
+
+  def decryption_indices_in_alphabet_array(message)
+    subtract_shift_from_indices(message).map do |index|
+      if index < -26
+        index % 27
+      elsif index > -26 && index.negative?
+        27 - index.abs
+      else
+        index
+      end
+    end
   end
 end
