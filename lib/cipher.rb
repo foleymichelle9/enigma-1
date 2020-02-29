85577e7262f04f4e7d79457767d771cf59e57427
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 14:25:14 -0700

    Add generate_random_key to cipher class

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -2,4 +2,9 @@ class Cipher
   def alphabet
     ("a".."z").to_a << " "
   end
+
+  def generate_random_key
+    five_random_digits = (0..9).to_a.sample(5)
+    five_random_digits.join.to_i
+  end
 end
