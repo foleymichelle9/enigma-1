d13eb91b2f99ee97707856f8b28220aaa2a98343
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 14:26:13 -0700

    Add find_keys method to cipher class

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -7,4 +7,15 @@ class Cipher
     five_random_digits = (0..9).to_a.sample(5)
     five_random_digits.join.to_i
   end
+
+  def find_keys
+    @key = generate_random_key if @key.nil?
+
+    keys = {}
+    keys[:a_key] = @key.to_s[0..1].to_i
+    keys[:b_key] = @key.to_s[1..2].to_i
+    keys[:c_key] = @key.to_s[2..3].to_i
+    keys[:d_key] = @key.to_s[3..4].to_i
+    keys
+  end
 end
