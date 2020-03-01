bd25f9825c2a2a082bdfc8ac2d6b3a85073dc3ec
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 21:58:08 -0700

    Change instance variable names for readbility

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -6,12 +6,12 @@ require 'date'
 
 class DecryptTest < Minitest::Test
   def setup
-    @decrypt = Decrypt.new
+    @decryption = Decrypt.new
     @encryption = Encrypt.new
   end
 
   def test_it_exists
-    assert_instance_of Decrypt, @decrypt
+    assert_instance_of Decrypt, @decryption
   end
 
   def test_it_can_return_alphabet_array
@@ -45,11 +45,11 @@ class DecryptTest < Minitest::Test
       " "
     ]
 
-    assert_equal expected, @decrypt.alphabet
+    assert_equal expected, @decryption.alphabet
   end
 
   def test_it_can_find_keys
-    @decrypt.stubs(:generate_random_key).returns("02715")
+    @decryption.stubs(:generate_random_key).returns("02715")
     expected = {
       :a_key=>2,
       :b_key=>27,
@@ -57,7 +57,7 @@ class DecryptTest < Minitest::Test
       :d_key=>15
     }
 
-    assert_equal expected, @decrypt.find_keys
+    assert_equal expected, @decryption.find_keys
   end
 
   def test_it_can_find_offsets
@@ -69,12 +69,12 @@ class DecryptTest < Minitest::Test
       :d_offset=>5
     }
 
-    assert_equal expected, @decrypt.find_offsets
+    assert_equal expected, @decryption.find_offsets
   end
 
   def test_it_can_find_shifts
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @decrypt.stubs(:generate_random_key).returns("02715")
+    @decryption.stubs(:generate_random_key).returns("02715")
     expected = {
       :a_shift=>3,
       :b_shift=>27,
@@ -82,60 +82,60 @@ class DecryptTest < Minitest::Test
       :d_shift=>20
     }
 
-    assert_equal expected, @decrypt.find_shifts
+    assert_equal expected, @decryption.find_shifts
   end
 
   def test_it_can_find_message_indices
     expected = [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22]
     sym_expected = ["!", 10, 4, 3, 4, 17, "!", 26,  14, 7, 20, 11, 22, "!"]
 
-    assert_equal expected, @decrypt.find_message_indices("keder ohulw")
-    assert_equal sym_expected, @decrypt.find_message_indices("!keder! ohulw!")
+    assert_equal expected, @decryption.find_message_indices("keder ohulw")
+    assert_equal sym_expected, @decryption.find_message_indices("!keder! ohulw!")
   end
 
   def test_it_can_slice_indices
     expected = [[10, 4, 3, 4], [17, 26, 14, 7], [20, 11, 22]]
     sym_expected = [["!", 10, 4, 3], [4, 17, "!", 26],  [14, 7, 20, 11], [22, "!"]]
 
-    assert_equal expected, @decrypt.slice_indices("keder ohulw")
-    assert_equal expected, @decrypt.slice_indices("KEDER OHULW")
-    assert_equal sym_expected, @decrypt.slice_indices("!keder! ohulw!")
+    assert_equal expected, @decryption.slice_indices("keder ohulw")
+    assert_equal expected, @decryption.slice_indices("KEDER OHULW")
+    assert_equal sym_expected, @decryption.slice_indices("!keder! ohulw!")
   end
 
   def test_it_can_subtract_shifts_from_indices
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @decrypt.stubs(:generate_random_key).returns("02715")
+    @decryption.stubs(:generate_random_key).returns("02715")
     expected = [7, -23, -70, -16, 14, -1, -59, -13, 17, -16, -51]
     sym_expected = ["!", -17, -69, -17, 1, -10, "!", 6, 11, -20, -53, -9, 19, "!"]
 
-    assert_equal expected, @decrypt.subtract_shift_from_indices("keder ohulw")
-    assert_equal expected, @decrypt.subtract_shift_from_indices("KEDER OHULW")
-    assert_equal sym_expected, @decrypt.subtract_shift_from_indices("!keder! ohulw!")
+    assert_equal expected, @decryption.subtract_shift_from_indices("keder ohulw")
+    assert_equal expected, @decryption.subtract_shift_from_indices("KEDER OHULW")
+    assert_equal sym_expected, @decryption.subtract_shift_from_indices("!keder! ohulw!")
   end
 
   def test_it_can_find_decryption_indices_in_alphabet_array
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @decrypt.stubs(:generate_random_key).returns("02715")
+    @decryption.stubs(:generate_random_key).returns("02715")
     expected = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
     sym_expected = ["!", 10, 12, 10, 1, 17, "!", 6, 11, 7, 1, 18, 19, "!"]
 
-    assert_equal expected, @decrypt.decryption_indices_in_alphabet_array("keder ohulw")
-    assert_equal expected, @decrypt.decryption_indices_in_alphabet_array("KEDER OHULW")
-    assert_equal sym_expected, @decrypt.decryption_indices_in_alphabet_array("!keder! ohulw!")
+    assert_equal expected, @decryption.decryption_indices_in_alphabet_array("keder ohulw")
+    assert_equal expected, @decryption.decryption_indices_in_alphabet_array("KEDER OHULW")
+    assert_equal sym_expected, @decryption.decryption_indices_in_alphabet_array("!keder! ohulw!")
   end
 
   def test_it_can_find_decrypted_letters
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @decrypt.stubs(:generate_random_key).returns("02715")
+    @decryption.stubs(:generate_random_key).returns("02715")
 
-    assert_equal "hello world", @decrypt.find_decrypted_letters("keder ohulw")
-    assert_equal "hello world", @decrypt.find_decrypted_letters("KEDER OHULW")
-    assert_equal "!hello! world!", @decrypt.find_decrypted_letters("!hxeoo!tzojeg!")
+    assert_equal "hello world", @decryption.find_decrypted_letters("keder ohulw")
+    assert_equal "hello world", @decryption.find_decrypted_letters("KEDER OHULW")
+    assert_equal "!hello! world!", @decryption.find_decrypted_letters("!hxeoo!tzojeg!")
   end
 
   def test_it_can_get_decryption_hash_creation
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    @decrypt.stubs(:generate_random_key).returns("02715")
+    @decryption.stubs(:generate_random_key).returns("02715")
     expected = {
       decryption: "hello world",
       key: "02715",
@@ -147,9 +147,9 @@ class DecryptTest < Minitest::Test
       date: "040895"
     }
 
-    assert_equal expected, @decrypt.decryption_hash_creation("keder ohulw")
-    assert_equal expected, @decrypt.decryption_hash_creation("KEDER OHULW")
-    assert_equal sym_expected, @decrypt.decryption_hash_creation("!hxeoo!tzojeg!")
+    assert_equal expected, @decryption.decryption_hash_creation("keder ohulw")
+    assert_equal expected, @decryption.decryption_hash_creation("KEDER OHULW")
+    assert_equal sym_expected, @decryption.decryption_hash_creation("!hxeoo!tzojeg!")
   end
 
   def test_it_can_decrypt_message_with_key_and_date
@@ -164,9 +164,9 @@ class DecryptTest < Minitest::Test
       date: "040895"
     }
 
-    assert_equal expected, @decrypt.decrypt("keder ohulw", "02715", "040895")
-    assert_equal expected, @decrypt.decrypt("KEDER OHULW", "02715", "040895")
-    assert_equal sym_expected, @decrypt.decrypt(("!hxeoo!tzojeg!"), "02715", "040895")
+    assert_equal expected, @decryption.decrypt("keder ohulw", "02715", "040895")
+    assert_equal expected, @decryption.decrypt("KEDER OHULW", "02715", "040895")
+    assert_equal sym_expected, @decryption.decrypt(("!hxeoo!tzojeg!"), "02715", "040895")
   end
 
   def test_it_can_decrypt_message_with_key
@@ -178,6 +178,6 @@ class DecryptTest < Minitest::Test
       date: "040895"
     }
 
-    assert_equal expected, @decrypt.decrypt(encrypted[:encryption], "02715")
+    assert_equal expected, @decryption.decrypt(encrypted[:encryption], "02715")
   end
 end
