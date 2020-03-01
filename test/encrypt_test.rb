fa43546b7aea7c7c78fea0f083f2e7461e76ad6f
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-29 21:12:06 -0700

    Add to tests for strings with symbols

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -84,35 +84,43 @@ class EncryptTest < Minitest::Test
   end
 
   def test_it_can_find_message_indices
-    encryption_expected = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
+    expected = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
+    sym_expected = ["!", 7, 4, 11, 11, 14, "!", 26, 22, 14, 17, 11, 3, "!"]
 
-    assert_equal encryption_expected, @encrypt.find_message_indices("hello world")
-    assert_equal encryption_expected, @encrypt.find_message_indices("HELLO WORLD")
+    assert_equal expected, @encrypt.find_message_indices("hello world")
+    assert_equal expected, @encrypt.find_message_indices("HELLO WORLD")
+    assert_equal sym_expected, @encrypt.find_message_indices("!HELLO! WORLD!")
   end
 
   def test_it_can_slice_indices
-    encryption_expected = [[7, 4, 11, 11], [14, 26, 22, 14], [17, 11, 3]]
+    expected = [[7, 4, 11, 11], [14, 26, 22, 14], [17, 11, 3]]
+    sym_expected = [["!", 7, 4, 11], [11, 14, "!", 26], [22, 14, 17, 11], [3, "!"]]
 
-    assert_equal encryption_expected, @encrypt.slice_indices("hello world")
-    assert_equal encryption_expected, @encrypt.slice_indices("HELLO WORLD")
+    assert_equal expected, @encrypt.slice_indices("hello world")
+    assert_equal expected, @encrypt.slice_indices("HELLO WORLD")
+    assert_equal sym_expected, @encrypt.slice_indices("!HELLO! WORLD!")
   end
 
   def test_it_can_add_shift_to_indices
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
     @encrypt.stubs(:generate_random_key).returns("02715")
     expected = [10, 31, 84, 31, 17, 53, 95, 34, 20, 38, 76]
+    sym_expected = ["!", 34, 77, 31, 14, 41, "!", 46, 25, 41, 90, 31, 6, "!"]
 
     assert_equal expected, @encrypt.add_shift_to_indices("hello world")
     assert_equal expected, @encrypt.add_shift_to_indices("HELLO WORLD")
+    assert_equal sym_expected, @encrypt.add_shift_to_indices("!HELLO! WORLD!")
   end
 
   def test_it_can_find_encryption_indices_in_alphabet_array_range
     Date.stubs(:today).returns(Date.new(1995, 8, 4))
     @encrypt.stubs(:generate_random_key).returns("02715")
     expected = [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22]
+    sym_expected = ["!", 7, 23, 4, 14, 14, "!", 19, 25, 14, 9, 4, 6, "!"]
 
     assert_equal expected, @encrypt.encryption_indices_in_alphabet_array("hello world")
     assert_equal expected, @encrypt.encryption_indices_in_alphabet_array("HELLO WORLD")
+    assert_equal sym_expected, @encrypt.encryption_indices_in_alphabet_array("!HELLO! WORLD!")
   end
 
   def test_it_can_find_encryted_letters
@@ -121,6 +129,7 @@ class EncryptTest < Minitest::Test
 
     assert_equal "keder ohulw", @encrypt.find_encryted_letters("hello world")
     assert_equal "keder ohulw", @encrypt.find_encryted_letters("HELLO WORLD")
+    assert_equal "!hxeoo!tzojeg!", @encrypt.find_encryted_letters("!HELLO! WORLD!")
   end
 
   def test_it_can_create_encryption_hash_creation
@@ -131,9 +140,15 @@ class EncryptTest < Minitest::Test
       :key=>"02715",
       :date=>"040895"
     }
+    sym_expected = {
+      :encryption=>"!hxeoo!tzojeg!",
+      :key=>"02715",
+      :date=>"040895"
+    }
 
     assert_equal expected, @encrypt.encryption_hash_creation("hello world")
     assert_equal expected, @encrypt.encryption_hash_creation("HELLO WORLD")
+    assert_equal sym_expected, @encrypt.encryption_hash_creation("!HELLO! WORLD!")
   end
 
   def test_test_it_can_encrypt_message_with_key_and_date
