f8ffd1ff355ef6155b1b1725aaa5e6849dce2484
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-02 15:03:02 -0700

    Change key back to instance variable

--- lib/module/hashable.rb
+++ lib/module/hashable.rb
@@ -1,8 +1,8 @@
 module Hashable
-  def hash_creation(key1_name, collection, key)
+  def hash_creation(key1_name, collection)
     hash_name = {}
     hash_name[key1_name.to_sym] = collection
-    hash_name[:key] = key
+    hash_name[:key] = @key
     hash_name[:date] = @date
     hash_name
   end
