d7f2ce96aeee21ffc2789a9069604a3d239fe3cf
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-02 16:36:04 -0700

    Add key argument for subtract_shift_from_indices and pass in key to find_shifts

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -7,18 +7,18 @@ class Decryption < Cipher
     decryption_hash_creation(message)
   end
 
-  def subtract_shift_from_indices(message)
+  def subtract_shift_from_indices(message, key)
     slices = []
     sliced = slice_indices(message)
     sliced.map do |slice|
       slices << slice[0] if !slice[0].nil? && !slice[0].is_a?(Numeric)
-      slices << slice[0] - find_shifts[:a_shift] if !slice[0].nil? && slice[0].is_a?(Numeric)
+      slices << slice[0] - find_shifts(key)[:a_shift] if !slice[0].nil? && slice[0].is_a?(Numeric)
       slices << slice[1] if !slice[1].nil? && !slice[1].is_a?(Numeric)
-      slices << slice[1] - find_shifts[:b_shift] if !slice[1].nil? && slice[1].is_a?(Numeric)
+      slices << slice[1] - find_shifts(key)[:b_shift] if !slice[1].nil? && slice[1].is_a?(Numeric)
       slices << slice[2] if !slice[2].nil? && !slice[2].is_a?(Numeric)
-      slices << slice[2] - find_shifts[:c_shift] if !slice[2].nil? && slice[2].is_a?(Numeric)
+      slices << slice[2] - find_shifts(key)[:c_shift] if !slice[2].nil? && slice[2].is_a?(Numeric)
       slices << slice[3] if !slice[3].nil? && !slice[3].is_a?(Numeric)
-      slices << slice[3] - find_shifts[:d_shift] if !slice[3].nil? && slice[3].is_a?(Numeric)
+      slices << slice[3] - find_shifts(key)[:d_shift] if !slice[3].nil? && slice[3].is_a?(Numeric)
     end
     slices
   end
