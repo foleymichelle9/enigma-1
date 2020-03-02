f64ffa8cead3ba2ceb6d01c01d19771718a7a855
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-01 22:55:02 -0700

    Require enigma file

--- lib/runner/encrypt.rb
+++ lib/runner/encrypt.rb
@@ -1,5 +1,8 @@
+require_relative '../enigma'
+
 ARGV == ["message.txt", "encrypted.txt"]
 input = ARGV
+
 write_message = File.open('./lib/runner/message.txt', "w")
 write_message.write("hello world")
 write_message.close
