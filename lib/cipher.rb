e4be9838434d43c22b8bd868a3e6f05808c67078
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-02 16:01:51 -0700

    Remove instance variables from find_keys method, pass in key as argument

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -15,14 +15,12 @@ class Cipher
     five_random_digits.join.to_i
   end
 
-  def find_keys
-    @key = generate_random_key if @key.nil?
-
+  def find_keys(key)
     keys = {}
-    keys[:a_key] = @key.to_s[0..1].to_i
-    keys[:b_key] = @key.to_s[1..2].to_i
-    keys[:c_key] = @key.to_s[2..3].to_i
-    keys[:d_key] = @key.to_s[3..4].to_i
+    keys[:a_key] = key.to_s[0..1].to_i
+    keys[:b_key] = key.to_s[1..2].to_i
+    keys[:c_key] = key.to_s[2..3].to_i
+    keys[:d_key] = key.to_s[3..4].to_i
     keys
   end
 
