16d4acf45a845f7e0484ef9dc5d8d827a28872c9
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-02 14:44:44 -0700

    Remove key as instance variable, add it as an argument

--- lib/module/hashable.rb
+++ lib/module/hashable.rb
@@ -1,8 +1,8 @@
 module Hashable
-  def hash_creation(key1_name, collection)
+  def hash_creation(key1_name, collection, key)
     hash_name = {}
     hash_name[key1_name.to_sym] = collection
-    hash_name[:key] = @key
+    hash_name[:key] = key
     hash_name[:date] = @date
     hash_name
   end
