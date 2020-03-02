ec2af99216ceaa62b3f8247b4c744d398314ce40
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-01 23:41:01 -0700

    Remove unnecessary lines and edit output index to accurately match file being written to

--- lib/runner/encrypt.rb
+++ lib/runner/encrypt.rb
@@ -2,12 +2,7 @@ require_relative '../enigma'
 
 enigma = Enigma.new
 
-ARGV == ["message.txt", "encrypted.txt"]
 input = ARGV
-
-# write_message = File.open('./lib/runner/message.txt', "w")
-# write_message.write("hello world")
-# write_message.close
 
 read_message = File.open('./lib/runner/message.txt', "r")
 message = read_message.read
@@ -20,4 +15,4 @@ write_encryption.close
 
 File.open('./lib/runner/encrypted.txt', "r")
 
-puts "Created #{input[0]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
+puts "Created #{input[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
