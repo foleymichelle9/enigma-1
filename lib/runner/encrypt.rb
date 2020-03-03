fa3d3009e59a8b785fd8e6e49809e5fe1706afc5
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-03 09:52:47 -0700

    Edit CLI to allow variable file reading

--- lib/runner/encrypt.rb
+++ lib/runner/encrypt.rb
@@ -3,16 +3,17 @@ require_relative '../enigma'
 enigma = Enigma.new
 
 input = ARGV
+message_to_read = input[0]
 
-read_message = File.open('./lib/runner/message.txt', "r")
+read_message = File.open(message_to_read, "r")
 message = read_message.read
 
 encrypted_message = enigma.encrypt(message)
 
-write_encryption = File.open('./lib/runner/encrypted.txt', "w")
+write_encryption = File.open(input[1], "w")
 write_encryption.write(encrypted_message[:encryption])
 write_encryption.close
 
-File.open('./lib/runner/encrypted.txt', "r")
+File.open(input[1], "r")
 
 puts "Created #{input[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}."
